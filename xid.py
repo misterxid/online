90d7e08f12cb1b3a27f17cf02836f5330bb214c4
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-27 12:26:13 +0700

    Create xid.py

---
+++ xid.py
@@ -0,0 +1,120 @@
+#mau reqod
+#malulah dengan dirimu
+
+figlet MR_XID  | lolcat
+ 
+echo                      " [•]===><===<>===><===<>===><===<>==><==[]"|lolcat
+echo                      " [•]Author   :  MR_XiD                  []"|lolcat
+echo                      " [•]Team     : JATIM BLACKHAT           []"|lolcat
+echo                      " [•]Note     : Jangan reqode ya         []"|lolcat
+echo                      " [•]Thansk to: All member               []"|lolcat
+echo                      " [•]thanks to: D35TR0Y SQUAD            []"|lolcat
+echo                      " [•]Note2    : Copyright©MR_XID 2018    []"
+echo                      " [•]===><==<>===><===<>===><===<>===><==[]"|lolcat
+
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " 1. TOOLS ISLAM "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " 2. BOM SPAM "   
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " 3. HAMMER DDOS "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " 4. SQL INJEK"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo.   " 5. SPAM WHATSAPP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" sf;
+
+if [ $sf = 1 ] || [ $sf = 1 ]
+then
+echo    "==========================="|lolcat
+echo    " 1. info tools "
+echo    "==========================="|lolcat
+echo    " 2. tanda tanda kiamat "
+echo    "===========================" | lolcat
+echo    " 3. pengertian hukum karma"
+echo    "===========================" | lolcat
+echo    "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" fri;
+fi
+if [ $fri = 1 ] || [ $fri = 1 ]
+then
+echo "-gunakan tool saya dengan benar-" | lolcat
+sleep 1
+echo "autor by MR_XID 2018 "
+echo " thansk to "
+echo "JATIM BLACKHAT " | lolcat
+echo "D35TR0Y SQUAD" | lolcat
+echo "I T A " | lolcat
+echo "L4EFY " | lolcat
+echo "ELITE MUSLIM CYBER ARMY " | lolcat
+sleep 1
+fi 
+if [ $fri = 2 ] || [ $fri = 2 ]
+then
+echo " 1. Anak-anak menjadi penuh kemarahan "
+echo " - Dengan kata lain, mereka berani melawan orangtua mereka " | lolcat
+echo " - Mereka tidak segan-segan berkata kasar dan menghardik orangtua mereka " | lolcat
+sleep 2
+echo " 2. Hujan dapat membakar"
+echo "-Jika kita menganalisisnya, hal ini telah terjadi. Sekarang hujan asam semakin sering terjadi " | lolcat
+sleep 1
+echo " 3. Orang-orang jahat bertebaran di muka bumi. "
+sleep 1 
+echo "- sekarang banyak orang yang jahat " | lolcat
+echo "-di katakan dalam" | lolcat
+echo "-kebaikan dibalas dengan kejahatan" | lolcat
+echo " 4. Orang Berkhianat Dipercaya "
+echo "-Orang-orang akan mempercayai orang-orang yang berkhianat, sedangkan orang-orang yang dapat dipercaya dianggap sebagai pengkhianat." | lolcat
+echo " 5. sekian itu aja yang yang gua sampaikan dan tunggu update tools nya lagi "
+fi 
+if [ $fri = 3 ] || [ $fri = 3 ]
+then
+echo " pengertian hukum karma bagi islam " | lolcat
+sleep 3
+echo "1.Merupakan hukum sebab-akibat, ada aksi dan ada reaksi. Jika berbuat baik, maka akan mendapat balasan baik, jika berbuat buruk maka akan mendapatkan balasan buruk juga "
+sleep 3
+echo "2.Menebak hal ghaib, semisal menebak bahwa engkau mendapatkan hal buruk ini karena perbuatan burukmu yang itu (disebutkan perbuatannya) "
+sleep 3
+echo "itulah hukum karma bagi islam "
+sleep 1
+echo "sekian Asalamualaikum"
+fi 
+if [ $sf = 2 ] || [ $sf = 2 ]
+then 
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sleep 1
+sh LITESPAM.sh
+echo "langsung santet mantan :v" | lolcat
+fi
+if [ $sf = 3 ] || [ $sf = 3 ]
+then 
+git clone https://github.com/cyweb/hammer 
+cd hammer 
+chmod 7777 hammer.py
+sleep 1
+python hammer.py 
+fi
+if [ $sf = 4 ] || [ $sf = 4 ]
+then 
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py
+echo "terinstall bos " | lolcat
+fi 
+[ $sf = 5 ] || [ $sf = 5 ]
+then 
+Pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/siputra12/prank
+cd prank
+pkg install php
+sleep 1
+php wa.php
+echo "terinstall cokkk "|lolcat
+sleep
+exit
+fi 
